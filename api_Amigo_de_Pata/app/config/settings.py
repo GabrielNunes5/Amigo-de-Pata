974b7ad6ecbd09b35028f8e559ce1e786c619329
Emanuel Maia <emanuelmaiadev@gmail.com>
2024-10-26 20:51:37 -0300

    Delete api_Amigo_de_Pata directory

--- api_Amigo_de_Pata/app/config/settings.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-class Config:
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        'DATABASE_URI',
-        'mysql+mysqlconnector://root:@localhost/amigo_de_pata'
-    )
